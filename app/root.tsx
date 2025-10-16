9c53a5f60787840713f6ea101eec26b027861de6
ryn <fdr160102@gmail.com>
2025-10-16 23:09:30 +0000

    gg diff jungla

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <main className="container">{children}</main>
         <ScrollRestoration />
         <Scripts />
       </body>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -8,25 +8,27 @@ export async function action({ request }: Route.ActionArgs) {
 
 const Home = () => {
   return (
-    <div>
-      <form method="post" encType="multipart/form-data">
-        <label htmlFor="outputFormat">Select output format:</label>
-        <input
-          type="text"
-          id="outputFormat"
-          name="outputFormat"
-          placeholder='e.g. [{"sheetName":"Expenses","columns":["Date","Amount","Category"]}]'
-          required
-        />
-        <label htmlFor="files">Upload files:</label>
-        <input type="file" id="files" name="files" multiple />
-        <textarea
-          name="instructions"
-          placeholder="Additional instructions (optional)"
-        ></textarea>
-        <input type="submit" value="Parse Receipts" />
-      </form>
-    </div>
+    <form
+      method="post"
+      encType="multipart/form-data"
+      className="flex flex-col p-2 "
+    >
+      <label htmlFor="outputFormat">Select output format:</label>
+      <input
+        type="text"
+        id="outputFormat"
+        name="outputFormat"
+        placeholder='e.g. [{"sheetName":"Expenses","columns":["Date","Amount","Category"]}]'
+        required
+      />
+      <label htmlFor="files">Upload files:</label>
+      <input type="file" id="files" name="files" multiple />
+      <textarea
+        name="instructions"
+        placeholder="Additional instructions (optional)"
+      ></textarea>
+      <input type="submit" value="Parse Receipts" />
+    </form>
   );
 };
 
